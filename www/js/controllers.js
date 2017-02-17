e7931e96af908ed40c1011df47d44c8eb4384d18
MAO\mao <364562354@qq.com>
2017-02-17 18:14:33 +0800

    更改模块名称;更新gitignore

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,9 +2,9 @@ angular.module('app.controllers', []);
 
 angular.module('app.controllers')
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', ['$scope', function($scope) {}])
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', ['$scope', 'Chats', function($scope, Chats) {
 	// With the new view caching in Ionic, Controllers are only called
 	// when they are recreated or on app start, instead of every page change.
 	// To listen for when this page is active (for example, to refresh data),
@@ -17,14 +17,14 @@ angular.module('app.controllers')
 	$scope.remove = function(chat) {
 		Chats.remove(chat);
 	};
-})
+}])
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+.controller('ChatDetailCtrl', ['$scope', '$stateParams', 'Chats', function($scope, $stateParams, Chats) {
 	$scope.chat = Chats.get($stateParams.chatId);
-})
+}])
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', ['$scope', function($scope) {
 	$scope.settings = {
 		enableFriends: true
 	};
-});
+}]);
