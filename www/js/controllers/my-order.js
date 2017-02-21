64a8cebd5d3da3245c16b4ad18080bcfce9798c4
ld <1538755010@qq.com>
2017-02-21 15:19:52 +0800

    新增我的预约页面-UI

--- www/js/controllers/my-order.js
+++ www/js/controllers/my-order.js
@@ -2,15 +2,33 @@
  * Created by liudong on 17-2-18.
  */
 angular.module('app.controllers')
-  .controller('AddManCtrl', ['$scope','$state', function($scope,$state) {
-    $scope.sex = [
-      {text: "男", value: "0"},
-      {text: "女", value: "1"},
-    ];
-    $scope.data = {
-      value: '0'
-    };
-    $scope.onAddManButtonClick=function () {
-
+  .controller('MyOrderCtrl', ['$scope', function($scope) {
+    $scope.noPayOrders=[
+      {
+        contant: '2016-12-02 11:25 邓超',
+        doctor: '口腔科 孙医生（工号007）',
+        state: '加急预约',
+        money: '￥0.01',
+      },
+    {
+      contant: '2016-12-03 11:25 陈赫',
+        doctor: '口腔科 孙医生（工号007）',
+      state: '加急预约',
+      money: '￥0.01',
     }
+    ];
+    $scope.payOrders=[
+      {
+        contant: '2016-12-02 11:25 邓超',
+        doctor: '口腔科 孙医生（工号007）',
+        state: '加急预约',
+        money: '￥0.01',
+      },
+      {
+        contant: '2016-12-03 11:25 陈赫',
+        doctor: '口腔科 孙医生（工号007）',
+        state: '加急预约',
+        money: '￥0.01',
+      }
+    ]
   }])
