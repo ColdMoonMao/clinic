dbdf717480016bc71cdbf36076f35b56d5b69f46
MAO\mao <364562354@qq.com>
2017-02-22 15:21:46 +0800

    完成注册接口调用;修复地图信息不在屏幕中心的问题;

--- www/js/controllers/register.js
+++ www/js/controllers/register.js
@@ -1,3 +1,16 @@
-angular.module('app.controllers').controller('RegisterCtrl', ['$scope', '$state', function($scope, $state) {
-
+angular.module('app.controllers').controller('RegisterCtrl', ['$scope', '$state','RegisterServe', function($scope, $state,RegisterServe) {
+	$scope.regObj = {
+		username: '', //string	y	用户名,
+		password: '', //string	y	密码,
+		sms_code: '', //string	y	短信验证码,
+		invitation_code:'' //string	y	邀请码
+	};
+	$scope.onRegister=function () {
+		RegisterServe.register($scope.regObj)
+			.then(function (data) {
+				console.log(data,'success');
+			},function (er) {
+				console.log(er,'error');
+			})
+	}
 }])
