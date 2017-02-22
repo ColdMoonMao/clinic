ac745784f1996ab39e7d6fc6ae9ff2ede7d1cf9c
MAO\mao <364562354@qq.com>
2017-02-22 17:20:16 +0800

    完成诊所地图页面接口;

--- www/js/controllers/clinic-detail.js
+++ www/js/controllers/clinic-detail.js
@@ -1,17 +1,22 @@
 angular.module('app.controllers')
-.controller('ClinicDetailCtrl', ['$scope', '$stateParams','ClinicDetailServe', function($scope, $stateParams,ClinicDetailServe) {
-	// $scope.clinic = Chats.get($stateParams.clinicId);
-	console.log($stateParams.clinicId);
-  var clinicParams={
-    token:sessionStorage.getItem('token'),
-    hospital_id:$stateParams.clinicId
-  }
-  ClinicDetailServe.detail(clinicParams)
-    .then(function (data) {
-      console.log(data,'success');
-      $scope.clinicDetailObj =data.data.result;
-    },function (er) {
-      console.log(er,'er');
-    })
-	
-}])
+	.controller('ClinicDetailCtrl', ['$scope', '$rootScope', '$stateParams', 'ClinicDetailServe', function($scope, $rootScope, $stateParams, ClinicDetailServe) {
+		// $scope.clinic = Chats.get($stateParams.clinicId);
+		console.log($stateParams.clinicId);
+		var clinicParams = {
+			token: sessionStorage.getItem('token'),
+			hospital_id: $stateParams.clinicId
+		}
+		ClinicDetailServe.detail(clinicParams)
+			.then(function(data) {
+				console.log(data, 'success');
+				$scope.clinicDetailObj = data.data.result;
+				$rootScope.clinicMapParams = {
+					longitude: $scope.clinicDetailObj.longitude,
+					latitude: $scope.clinicDetailObj.latitude,
+					name: $scope.clinicDetailObj.name,
+					address: $scope.clinicDetailObj.address
+				}
+			}, function(er) {
+				console.log(er, 'er');
+			})
+	}])
