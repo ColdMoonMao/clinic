432c011f309c61580a30c8a302b02f16912788ef
MAO\mao <364562354@qq.com>
2017-02-23 17:37:00 +0800

    注册页面加入提示信息与载入动画;修复获取验证码按钮不能点击的问题

--- www/js/controllers/register.js
+++ www/js/controllers/register.js
@@ -1,16 +1,70 @@
-angular.module('app.controllers').controller('RegisterCtrl', ['$scope', '$state','RegisterServe', function($scope, $state,RegisterServe) {
+angular.module('app.controllers').controller('RegisterCtrl', ['$scope', '$state', '$ionicLoading', 'RegisterServe', '$ionicPopup', function($scope, $state, $ionicLoading, RegisterServe, $ionicPopup) {
+	//scope变量声明
 	$scope.regObj = {
 		username: '', //string	y	用户名,
 		password: '', //string	y	密码,
 		sms_code: '', //string	y	短信验证码,
-		invitation_code:'' //string	y	邀请码
+		invitation_code: '' //string	y	邀请码
 	};
-	$scope.onRegister=function () {
-		RegisterServe.register($scope.regObj)
-			.then(function (data) {
-				console.log(data,'success');
-			},function (er) {
-				console.log(er,'error');
+	$scope.isChecked = true;
+	//scope函数声明
+	//注册协议同意按钮
+	$scope.onCheck = function() {
+			$scope.isChecked = !$scope.isChecked;
+			console.log($scope.isChecked);
+		}
+		//注册函数
+	$scope.onRegister = function() {
+			if ($scope.isChecked) {
+				console.log($scope.regObj);
+				$ionicLoading.show(); //动画
+				RegisterServe.register($scope.regObj)
+					.then(function(data) {
+						console.log(data, 'success');
+						$ionicLoading.hide(); //动画
+						//弹窗提示信息
+						$ionicPopup.alert({
+							title: '注册',
+							template: data.data.error.message,
+							okText: '确定'
+						}).then(function(res) {
+							// console.log(res);
+						})
+					}, function(er) {
+						console.log(er, 'error');
+						$ionicLoading.hide(); //动画
+					})
+			} else {
+				//弹窗提示信息
+				$ionicPopup.alert({
+					title: '注册',
+					template: '请同意注册协议',
+					okText: '确定'
+				}).then(function(res) {
+					// console.log(res);
+				})
+			}
+
+		}
+		//获取验证码按钮函数
+	$scope.onGetCode = function() {
+		$ionicLoading.show(); //动画
+		console.log($scope.regObj.username);
+		RegisterServe.sms_code({ phone: $scope.regObj.username })
+			.then(function(data) {
+				console.log(data, 'success');
+				// $scope.regObj.sms_code=;
+				$ionicLoading.hide(); //动画
+				//弹窗提示信息
+				$ionicPopup.alert({
+					title: '注册',
+					template: data.data.error.message,
+					okText: '确定'
+				}).then(function(res) {
+					console.log(res);
+				})
+			}, function(er) {
+				console.log(er, 'er');
 			})
 	}
 }])
--- www/js/services/register.js
+++ www/js/services/register.js
@@ -1,21 +1,36 @@
 // REGISTER页面服务
 angular.module('app.services')
-	.factory('RegisterServe', ['$http','$q',function($http, $q) {
-			return {
-				register: function(params) {
-					var deferred = $q.defer();
-					$http({
-						method: 'POST',
-						url: GlobalConfig.url.api.user.register,
-						params: params,
-						responseType: 'json',
-						timeout: 30000
-					}).then(function(data) {
-						deferred.resolve(data);
-					}, function(error) {
-						deferred.reject(error);
-					});
-					return deferred.promise;
-				},
-			};
-		}]);
+	.factory('RegisterServe', ['$http', '$q', function($http, $q) {
+		return {
+			register: function(params) {
+				var deferred = $q.defer();
+				$http({
+					method: 'POST',
+					url: GlobalConfig.url.api.user.register,
+					params: params,
+					responseType: 'json',
+					timeout: 30000
+				}).then(function(data) {
+					deferred.resolve(data);
+				}, function(error) {
+					deferred.reject(error);
+				});
+				return deferred.promise;
+			},
+			sms_code: function(params) {
+				var deferred = $q.defer();
+				$http({
+					method: 'GET',
+					url: GlobalConfig.url.api.message.sendRegister,
+					params: params,
+					responseType: 'json',
+					timeout: 30000
+				}).then(function(data) {
+					deferred.resolve(data);
+				}, function(error) {
+					deferred.reject(error);
+				});
+				return deferred.promise;
+			}
+		};
+	}]);
