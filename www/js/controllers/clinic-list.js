b10f610b92069739692830fee8b5d99990f97f24
MAO\mao <364562354@qq.com>
2017-02-23 17:57:57 +0800

    诊室列表加入动画

--- www/js/controllers/clinic-list.js
+++ www/js/controllers/clinic-list.js
@@ -1,10 +1,17 @@
-angular.module('app.controllers').controller('ClinicListCtrl', ['$scope','ClinicListServe',function($scope,ClinicListServe) {
-	ClinicListServe.list({token:sessionStorage.getItem('token')})
-		.then(function (data) {
-			console.log(data,'success');
-			$scope.clinicArr =data.data.result;
-		},function (er) {
-			console.log(er,'er');
+angular.module('app.controllers').controller('ClinicListCtrl', ['$scope', 'ClinicListServe', '$ionicLoading', function($scope, ClinicListServe, $ionicLoading) {
+	//scope变量声明
+	$scope.clinicArr = [];
+	//本controller函数
+	//获取推荐医生
+	$ionicLoading.show(); //动画
+	ClinicListServe.list({ token: sessionStorage.getItem('token') })
+		.then(function(data) {
+			console.log(data, 'success');
+			$ionicLoading.hide(); //动画
+			$scope.clinicArr = data.data.result;
+		}, function(er) {
+			console.log(er, 'er');
 		})
-	
+		//scope函数
+
 }])
--- www/js/controllers/tab-home.js
+++ www/js/controllers/tab-home.js
@@ -1,12 +1,18 @@
 angular.module('app.controllers')
 
-.controller('HomeCtrl', ['$scope','HomeServe', function($scope,HomeServe) {
-	//上传文件
-	HomeServe.getRecommend({token:sessionStorage.getItem('token')})
+.controller('HomeCtrl', ['$scope', 'HomeServe', '$ionicLoading', function($scope, HomeServe, $ionicLoading) {
+	//scope变量声明
+	$scope.recommendDoctorArr = [];
+	//本controller函数
+	//获取推荐医生
+	$ionicLoading.show(); //动画
+	HomeServe.getRecommend({ token: sessionStorage.getItem('token') })
 		.then(function(data) {
 			console.log(data, 'getRecommend succes');
-			$scope.recommendDoctorArr=data.data.result;
+			$ionicLoading.hide(); //动画
+			$scope.recommendDoctorArr = data.data.result;
 		}, function(er) {
 			console.log(er, 'getRecommend er');
 		});
+	//scope函数
 }])
