109c7e599ee8546e8a5008e738eab6f9a21ff629
ld <1538755010@qq.com>
2017-02-23 15:39:29 +0800

    家庭联系人以及个人信息页面服务完成，个人信息页面样式完善以及我的预约代码优化

--- www/js/controllers/household-contacts.js
+++ www/js/controllers/household-contacts.js
@@ -1,12 +1,19 @@
 /**
  * Created by liudong on 17-2-18.
  */
+//家庭联系人页面服务
 angular.module('app.controllers')
-  .controller('HouseholdContactsCtrl', ['$scope','Contacts','$state', function($scope,Contacts,$state) {
+  .controller('HouseholdContactsCtrl', ['$scope','$rootScope','Contacts','$state', function($scope,$rootScope,Contacts,$state) {
+    //接口参数
     $scope.contact = {
       token: sessionStorage.getItem('token'),         //	令牌
       normal_user_id: sessionStorage.getItem('userId'), //	用户id
     };
+
+    //数据绑定
+    $scope.peopleArr=null;
+    $rootScope.alterLIst=null;
+    //事件
     $scope.message=function () {
       Contacts.information($scope.contact)
         .then(function (data) {
@@ -20,11 +27,20 @@ angular.module('app.controllers')
 
 
 
-    // console.log($scope.peopleArr)
+    //跳转
     $scope.onAddButtonClick=function () {
       $state.go('tab.addMan')
     },
-      $scope.changePage=function () {
+      $scope.changePage=function (index) {
+      console.log(index);
+        $rootScope.alterLIst=[{
+          id:$scope.peopleArr[index].id,
+          name:$scope.peopleArr[index].name,
+          gender:$scope.peopleArr[index].gender,
+          identity_card:$scope.peopleArr[index].identity_card,
+          address:$scope.peopleArr[index].address,
+          phone:$scope.peopleArr[index].phone,
+        }]
         $state.go('tab.alterMan')
       }
   }])
